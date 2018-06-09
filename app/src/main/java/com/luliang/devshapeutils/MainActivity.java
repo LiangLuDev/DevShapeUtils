3dbec5c1b6022b585783aa1e22e8b8b9daae0899
LiangLuDev <927195249@qq.com>
2018-06-09 17:02:50 +0800

    创建dev分支
    Signed-off-by: LiangLuDev <927195249@qq.com>

--- app/src/main/java/com/luliang/devshapeutils/MainActivity.java
+++ app/src/main/java/com/luliang/devshapeutils/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        
     }
 
 
