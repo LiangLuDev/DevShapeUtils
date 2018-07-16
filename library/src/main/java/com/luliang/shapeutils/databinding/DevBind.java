bde70f8a5a780b2a253ad607751668f1835331f1
LuLiangDev <liangludev@gmail.com>
2018-07-16 19:05:14 +0800

    databinding shape设置-待完善

--- library/src/main/java/com/luliang/shapeutils/databinding/DevBind.java
+++ library/src/main/java/com/luliang/shapeutils/databinding/DevBind.java
@@ -1,6 +1,7 @@
 package com.luliang.shapeutils.databinding;
 
 import android.databinding.BindingAdapter;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.View;
@@ -57,12 +58,108 @@ public class DevBind {
                                  String gradientLinearOrientation, int gradientLinearStartColor, int gradientLinearEndColor,
                                  String gradientSweepColors, float gradientRadialRadius, String gradientRadialColors) {
 
+
         Drawable shapeDrawable = createShape(shape, solidColor, lineWidth, lineColor,
                 dashLineWidth, dashLineColor, dashWidth, dashGap,
                 radius, trRadius, tlRadius, brRadius, blRadius,
                 gradientLinearOrientation, gradientLinearStartColor, gradientLinearEndColor,
                 gradientSweepColors, gradientRadialRadius, gradientRadialColors);
+
 //        view.setBackground(DevShapeUtils.shape(DevShape.OVAL).bindSolid(solidColor).build());
+
+        //样式设置（圆形或者矩形）
+//        int shapeModel;
+//        if (shape == null) {
+//            shapeModel = DevShape.OVAL;
+//        } else {
+//            switch (shape) {
+//                case "RECTANGLE":
+//                    shapeModel = DevShape.RECTANGLE;
+//                    break;
+//                case "OVAL":
+//                default:
+//                    shapeModel = DevShape.OVAL;
+//                    break;
+//            }
+//        }
+//
+//        DevShape devShape = DevShapeUtils.shape(shapeModel);
+//
+//        //背景颜色
+//        if (solidColor != 0) {
+//            devShape.bindSolid(solidColor);
+//        }
+//        //边框宽度和颜色
+//        if (lineColor != 0 && lineWidth > 0) {
+//            devShape.bindLine(lineWidth, lineColor);
+//        }
+//
+//        //虚线边框
+//        if (dashLineWidth > 0 && dashLineColor != 0 && dashWidth > 0 && dashGap > 0) {
+//            devShape.bindDashLine(dashLineWidth, dashLineColor, dashWidth, dashGap);
+//        }
+//
+//        //圆角设置-4个方向圆角弧度
+//        if (radius > 0) {
+//            devShape.radius(radius);
+//        }
+//        //右上圆角
+//        if (trRadius > 0) {
+//            devShape.trRadius(trRadius);
+//        }
+//
+//        //左上圆角
+//        if (tlRadius > 0) {
+//            devShape.tlRadius(tlRadius);
+//        }
+//
+//        //右下圆角
+//        if (brRadius > 0) {
+//            devShape.brRadius(brRadius);
+//        }
+//        //左下圆角
+//        if (blRadius > 0) {
+//            devShape.blRadius(blRadius);
+//        }
+//
+//        //线性渐变方向设置
+//        if (gradientLinearOrientation == null) {
+//            gradientLinearOrientation = "TOP_BOTTOM";
+//        }
+//        //线性渐变颜色设置
+//        if (gradientLinearStartColor != 0 && gradientLinearEndColor != 0) {
+//            devShape.orientation(gradientLinearOrientation);
+//            devShape.bindGradientLinear(gradientLinearStartColor, gradientLinearEndColor);
+//        }
+//
+//        //扫描渐变（仅支持 #FFFFFFF 类格式颜色设置）
+//        if (gradientSweepColors != null) {
+//            if (gradientSweepColors.contains("|")) {
+//                String[] colors = "|".split(gradientSweepColors);
+//                for (String color : colors) {
+//                    if (!color.contains("#"))
+//                        throw new ExceptionInInitializerError("错误的颜色格式,仅支持 #FFFFFFF 类格式颜色设置");
+//                }
+//                devShape.gradientSweep(colors);
+//            } else {
+//                throw new ExceptionInInitializerError("渐变颜色数组至少需要两个颜色");
+//            }
+//        }
+//
+//        //辐射渐变（仅支持 #FFFFFFF 类格式颜色设置）
+//        if (gradientRadialRadius > 0 && gradientRadialColors != null) {
+//            if (gradientRadialColors.contains("|")) {
+//                String[] colors = "|".split(gradientRadialColors);
+//                for (String color : colors) {
+//                    if (!color.contains("#"))
+//                        throw new ExceptionInInitializerError("错误的颜色格式,仅支持 #FFFFFFF 类格式颜色设置");
+//                }
+//                devShape.gradientRadial(gradientRadialRadius, colors);
+//            } else {
+//                throw new ExceptionInInitializerError("渐变颜色数组至少需要两个颜色");
+//            }
+//        }
+
         //设置样式背景
         view.setBackground(shapeDrawable);
     }
@@ -99,8 +196,8 @@ public class DevBind {
 
         DevShape devShape = DevShapeUtils.shape(shapeModel);
 
-        Log.d(TAG, "createShape: " + solidColor);
-        devShape.bindSolid(solidColor);
+        Log.d(TAG, "createShape: " + toHexEncoding(solidColor));
+        devShape.solid(toHexEncoding(solidColor));
 
         //背景颜色
 //        if (solidColor != null) {
@@ -203,6 +300,28 @@ public class DevBind {
      */
     private static boolean isStrColor(Object strColor) {
         return strColor.toString().contains("#");
+    }
+
+    /**
+     * Android色值（例：-16777216）转换为16进制
+     *
+     * @param color
+     * @return
+     */
+    private static String toHexEncoding(int color) {
+        String R, G, B;
+        StringBuffer sb = new StringBuffer();
+        R = Integer.toHexString(Color.red(color));
+        G = Integer.toHexString(Color.green(color));
+        B = Integer.toHexString(Color.blue(color));
+        R = R.length() == 1 ? "0" + R : R;
+        G = G.length() == 1 ? "0" + G : G;
+        B = B.length() == 1 ? "0" + B : B;
+        sb.append("#");
+        sb.append(R.toUpperCase());
+        sb.append(G.toUpperCase());
+        sb.append(B.toUpperCase());
+        return sb.toString();
     }
 
 }
